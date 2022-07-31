5561a7c5869015983c7a1d88e773c136e98a3273
Derek Mason <3derekmason@gmail.com>
2022-07-31 11:51:18 -0600

    express init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = require("./routes/index.js");
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+const app = express();
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use("/api/", router);
+
+app.listen(PORT, () => {
+  console.log(`Connected on port: ${PORT}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const controller = require("./controllers/index.js");
+
+const router = require("express").Router();
+
+module.exports = router;
